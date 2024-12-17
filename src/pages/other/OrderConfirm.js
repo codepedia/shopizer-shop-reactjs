333afadef8b3df9e10a8b4adb96ba346af127013
Jaimin Patel <1994.jaimin@gmail.com>
2024-12-17 23:26:52 +0530

    - Design the order confirm page - Fixed the recent page image showing issue

--- src/pages/other/OrderConfirm.js
+++ src/pages/other/OrderConfirm.js
@@ -1,16 +1,53 @@
 import PropTypes from "prop-types";
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import React, { Fragment, useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
 import MetaTags from "react-meta-tags";
 import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import Layout from "../../layouts/Layout";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
 import { connect } from "react-redux";
 import { multilanguage } from "redux-multilanguage";
-import { getLocalData } from "../../util/helper";
-const OrderConfirm = ({ location, orderID, strings, merchant }) => {
-  const { pathname } = location;
+import { setLoader } from "../../redux/actions/loaderActions";
+import constant from '../../util/constant';
+import WebService from '../../util/webService';
+import { isValidObject } from "../../util/helper";
 
+const OrderConfirm = ({ location, orderID, strings, merchant, setLoader }) => {
+  const { pathname } = location;
+  const [orderDetails, setorderDetails] = useState({});
+  const [subTotal, setSubTotal] = useState(0);
+  const [shipping, setShipping] = useState(0);
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    // if (orderID) {
+    getOrderDetails()
+    // }
+  }, [orderID])
+  const getOrderDetails = async () => {
+    setLoader(true)
+    let action = constant.ACTION.AUTH + constant.ACTION.ORDERS + orderID;
+    try {
+      let response = await WebService.get(action);
+      if (response) {
+        // console.log(response)
+        setorderDetails(response)
+        response.totals.map((val) => {
+          if (val.module == 'subtotal') {
+            setSubTotal(val.value)
+          } else if (val.module == 'shipping') {
+            setShipping(val.value)
+          } else if (val.module == 'total') {
+            setTotal(val.value)
+          }
+        })
+        // // setConfig(response)
+      }
+      setLoader(false)
+    } catch (error) {
+      setLoader(false)
+      console.log(error, '------------')
+    }
+  }
   return (
     <Fragment>
       <MetaTags>
@@ -30,28 +67,82 @@ const OrderConfirm = ({ location, orderID, strings, merchant }) => {
         {/* breadcrumb */}
         <Breadcrumb />
         <div className="error-area pt-40 pb-40">
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-xl-7 col-lg-8 text-center">
-                <div className="error">
-                  <h3>{strings["Order Completed"]}</h3>
-                  <h2>{strings["Thank you for ordering from importa"]}</h2>
-                  <p>
-                    {strings["Your order id is"]} <b>{orderID}</b> <br />{strings["An email with you order details has been sent to"]} {getLocalData('order-email')}
-                  </p>
-                  <p>
-                    {strings["Feedback"]}
-                  </p>
-                  <Link to={"/"} className="error-btn">
-                    {strings["Shop Now"]}
-                  </Link>
+          {isValidObject(orderDetails) ?
+            <div className="container confirm-details-container">
+              <div class="thank-you-section">
+
+                <h1><img src="assets/img/confirm.gif" /> Thank you, your order has been placed.</h1>
+                <div style={{ padding: '20px 0' }}><p>Your order will be processed within 24 hours during working days. We will notify you by email once your order has been shipped.</p></div>
+                <div class="billing-info">
+                  <h3>Billing address</h3>
+                  <table>
+
+                    <tr>
+                      <td><p><strong>Name</strong></p></td>
+                      <td><p style={{ marginLeft: '40px' }}>{orderDetails?.billing?.firstName} {orderDetails?.billing?.lastName}</p></td>
+                    </tr>
+                    <tr>
+                      <td><p><strong>Address</strong></p></td>
+                      <td><p style={{ marginLeft: '40px' }}>{orderDetails?.billing?.address},  {orderDetails?.billing?.city} {orderDetails?.billing?.zone} {orderDetails?.billing?.country}, {orderDetails?.billing?.postalCode}</p></td>
+                    </tr>
+                    <tr>
+                      <td><p><strong>Phone</strong></p></td>
+                      <td><p style={{ marginLeft: '40px' }}>{orderDetails?.billing?.phone}</p></td>
+                    </tr>
+                    <tr>
+                      <td><p><strong>Email</strong></p></td>
+                      <td><p style={{ marginLeft: '40px' }}>{orderDetails?.billing?.email}</p></td>
+                    </tr>
+                  </table>
+                  {/* <p><strong>Name</strong>: Jane Smith</p>
+                <p><strong>Address</strong>: 456 Oak St #3b, San Francisco, CA 94102, United States</p>
+                <p><strong>Phone</strong>: +1 (415) 555-1234</p>
+                <p><strong>Email</strong>: jane.smith@email.com</p> */}
+                </div>
+                <button class="track-button">Track Your Order</button>
+              </div>
+
+              <div class="order-summary">
+                <h2>Order Summary</h2>
+                <div class="order-details">
+                  <div class="info row">
+                    <p className="col-4 info-details"><strong>Date</strong><br />{orderDetails?.datePurchased}</p>
+                    <p className="col-4 info-details"><strong>Order Number</strong><br />{orderDetails?.id}</p>
+                    <p className="col-4"><strong>Payment Method</strong><br />{orderDetails?.paymentType}</p>
+                  </div>
+                  <div style={{ padding: '15px 0', borderBottom: '1px solid lightgrey' }}>
+                    {
+                      orderDetails?.products?.map((orderItem, key) => {
+                        return (<div class="product">
+                          <img src={orderItem?.product?.image?.imageUrl} alt="All In One Chocolate Combo" />
+                          <div class="product-details">
+                            <p><strong>{orderItem?.productName}</strong></p>
+                            <p>Qty: {orderItem?.orderedQuantity}</p>
+                          </div>
+                          <p class="price">{orderItem?.subTotal}</p>
+                        </div>)
+                      })
+                    }
+
+                  </div>
+                  <div class="pricing">
+                    <p><b>Sub Total</b><span>${subTotal}</span></p>
+                    <p><b>Shipping ({orderDetails?.shippingModule})</b><span>${shipping}</span></p>
+                    {/* <p>Tax<span>$5.00</span></p> */}
+                  </div>
+
+                  <div class="total">
+                    <p><strong>Order Total</strong><span><strong>${total}</strong></span></p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+            :
+            <>Plese Wait</>
+          }
         </div>
       </Layout>
-    </Fragment>
+    </Fragment >
   );
 };
 
@@ -65,5 +156,12 @@ const mapStateToProps = state => {
     merchant: state.merchantData.merchant
   };
 };
-export default connect(mapStateToProps, null)(multilanguage(OrderConfirm));
+const mapDispatchToProps = dispatch => {
+  return {
+    setLoader: (value) => {
+      dispatch(setLoader(value));
+    }
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(multilanguage(OrderConfirm));
 // export default OrderConfirm;
