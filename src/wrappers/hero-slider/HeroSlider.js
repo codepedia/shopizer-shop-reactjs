4f7b7a7f317a19dc0c45b418dd0ee47873f7609a
Jaimin Patel <1994.jaimin@gmail.com>
2025-01-13 12:15:46 +0530

    EC-15 Designed the slider section & integrated the API

--- src/wrappers/hero-slider/HeroSlider.js
+++ src/wrappers/hero-slider/HeroSlider.js
@@ -1,33 +1,86 @@
-import React from "react";
-// import PropTypes from "prop-types";
-import HeroSliderStatic from "../../components/hero-slider/HeroSliderStatic.js";
-import { multilanguage } from "redux-multilanguage";
-import { connect } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import Swiper from "react-id-swiper";
+// import sliderData from "../../data/hero-sliders/hero-slider-fifteen.json";
+import HeroSliderSingle from "../../components/hero-slider/HeroSliderSingle.js";
+import WebService from '../../util/webService';
+import constant from '../../util/constant';
+const HeroSlider = ({ }) => {
 
-const HeroSlider = ({ string }) => {
+  const [sliderText, setSliderText] = useState('')
+  const [sliderData, setSliderData] = useState([])
 
+  useEffect(() => {
+    getBannerImage();
+    getBannerText();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const getBannerImage = async () => {
+    let action = constant.ACTION.CONTENT + constant.ACTION.IMAGES;
+    try {
+      let response = await WebService.get(action);
+      console.log(response);
+      if (response) {
+        setSliderData(response.content);
+      }
+    } catch (error) {
+    }
+  }
+  const getBannerText = async () => {
+    let action = constant.ACTION.CONTENT + constant.ACTION.BOXES + constant.ACTION.BANNER_TEXT;
+    try {
+      let response = await WebService.get(action);
+      console.log(response);
+      if (response) {
+        setSliderText(response);
+      }
+    } catch (error) {
+    }
+  }
+  const params = {
+    effect: "fade",
+    loop: true,
+    speed: 1000,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    watchSlidesVisibility: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    // renderPrevButton: () => (
+    //   <button className="swiper-button-prev ht-swiper-button-nav">
+    //     <i className="pe-7s-angle-left" />
+    //   </button>
+    // ),
+    // renderNextButton: () => (
+    //   <button className="swiper-button-next ht-swiper-button-nav">
+    //     <i className="pe-7s-angle-right" />
+    //   </button>
+    // )
+  };
   return (
-    <div className="site-blocks-cover">
+    <div className="slider-area">
+      <div className="slider-active nav-style-1">
       <div className="container">
-        <HeroSliderStatic
-          pitch1={string["Pitch1"]}
-          pitch2={string["Pitch2"]}
-          pitch3={string["Shop now"]}
-        />
+        <Swiper {...params}>
+          {sliderData &&
+            sliderData.map((single, key) => {
+              return (
+                <HeroSliderSingle
+                  data={single}
+                  key={key}
+                  sliderText={sliderText}
+                  sliderClass="swiper-slide"
+                />
+              );
+            })}
+        </Swiper>
+        </div>
       </div>
     </div>
   );
-
-
 };
 
-const mapStateToProps = state => {
-
-  return {
-    currentLanguageCode: state.multilanguage.currentLanguageCode,
-    defaultStore: state.merchantData.defaultStore,
-    isLoading: state.loading.isLoading
-  }
-}
-
-export default connect(mapStateToProps)(multilanguage(HeroSlider));
+export default HeroSlider;
